617045c71db51c056a31523a733c91d30b57e27d
Andrin Weiler <andrin.weiler@smoca.ch>
2021-01-15 16:31:41 +0100

    removed setRtc

--- m5stack-scd30.ino
+++ m5stack-scd30.ino
@@ -1183,30 +1183,6 @@ void printTime() {
 
 }
 
-void setRtc() {
-  configTime(0, 0, "pool.ntp.org");
-
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
-    return;
-  }
-
-  RTC_TimeTypeDef rtctime;
-  RTC_DateTypeDef rtcdate;
-
-  rtctime.Seconds = timeinfo.tm_sec;
-  rtctime.Minutes = timeinfo.tm_min;
-  rtctime.Hours = timeinfo.tm_hour;
-  rtcdate.Year = timeinfo.tm_year + 1900;
-  rtcdate.Month = timeinfo.tm_mon + 1;
-  rtcdate.Date = timeinfo.tm_mday;
-
-  M5.Rtc.SetDate(&rtcdate);
-  M5.Rtc.SetTime(&rtctime);
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-
 void setTimeFromRtc() {
   setenv("TZ", "UTC", 1);
   tzset();
